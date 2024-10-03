55fd218f10b8636e5d2f39a6210afb430398f274
Mykola Chernyashevskyy <chernya2@illinois.edu>
2024-10-03 10:38:43 -0400

    Ended up fixing the appropriate axes in the code.

--- analysis.m
+++ analysis.m
@@ -7,21 +7,66 @@ filePath = './Ag111_360nm_Data.xlsx';
 % Read the Excel file into a matrix
 data = readmatrix(filePath);
 
+[rows, cols] = size(data);
+
+disp(rows);
+disp(cols);
+
+% Generate Energy Axis (electronVolts)
+startValue = 6.26;
+increment = 0.0121569;
+numPoints = 256;
+endValue = startValue + (numPoints - 1) * increment;
+
+energyAxisEv = linspace(startValue, endValue, numPoints);
+
+% Generate Time Axis (femptoseconds)
+startValue = -73.1844;
+increment = 0.034375;
+numPoints = 4352;
+endValue = startValue + (numPoints - 1) * increment;
+
+timeAxisFs = linspace(startValue, endValue, numPoints);
+
 % Create a 2D matrix for the fast fourier transform data
 fftData = zeros(size(data));
 
+% Plot the raw data with appropriate axes.
 figure;
-plot(data(1,:));
+imagesc(timeAxisFs, energyAxisEv, data);
+colorbar;
+
+% Plot the first row of the data matrix
+figure;
+plot(timeAxisFs, data(1,:));
 
 % for loop runs through all the rows of the code. 
 % argument of the for loop stores the fast fourier transformed row into a row of the fftData matrix.
 
-for i = 1:size(data, 1)
-    fftData(i,:) = fft(data(i,:));
-end
+% for i = 1:size(data, 1)
+%     fftData(i,:) = fft(data(i,:));
+% end
 
+% Plot the first row of the fast fourier transformed data matrix
 figure;
-plot(abs(fftshift(data(1,:))));
+plot(abs(fft(data(1,:))));
+
+% Plot the first row of the fast fourier transformed and shifted data matrix
+figure;
+plot(abs(fftshift(fft(data(1,:)))));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 % figure;
 % imagesc(data);
